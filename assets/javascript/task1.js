5c07c5b1642819908d720764854d701ac959e2cb
Uriahx <uriah.squarcina@vtsd.com>
2024-01-11 11:47:15 -0500

    boh

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,1 +1,11 @@
 /* Put code for the task below */
+var imageTask1 = document.getElementById('imageTask1');
+var buttonTask1 = document.getElementById('buttonTask1');
+
+function showButton() {
+    buttonTask1.style.display = 'block';
+}
+
+function hideImage() {
+    imageTask1.style.display = 'none';
+}
